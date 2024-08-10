cb1b31e0cc53f82c3e09fb318cf1c5a9d0bb0901
MGilangChandra <gilangchan2107@gmail.com>
2024-08-10 17:04:45 +0700

    Add: Model and Migration
    Siswa, WaliKelas, KepalaJurusan, Kelas, Jurusan, TahunAjaran

--- database/migrations/2024_08_10_165352_create_kepala_jurusans_table.php
+++ database/migrations/2024_08_10_165352_create_kepala_jurusans_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pegawais', function (Blueprint $table) {
+        Schema::create('kepala_jurusans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('jurusan_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pegawais');
+        Schema::dropIfExists('kepala_jurusans');
     }
 };
---
+++ database/migrations/2024_08_10_167001_create_siswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->string('nis', 20)->primary();
+            $table->string('nama_lengkap', 50);
+            $table->enum('jenis_kelamin', ['Laki-Laki', 'Perempuan']);
+            $table->date('tanggal_lahir');
+            $table->text('alamat')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('no_telp', 16)->nullable();
+            $table->string('foto')->nullable();
+            $table->foreignId('kelas_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
